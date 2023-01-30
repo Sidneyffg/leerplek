9e75bef14fdd3fecca21f73fdda91c8496ffc2f5
Sidneyffg <sidney.oostveen@gmail.com>
2023-01-30 21:42:26 +0100

    new update page

---
+++ website/static/class.js
@@ -0,0 +1,34 @@
+let setSelected = false;
+function createUpdateTypeChange(e) {
+    if (e.value == 2) {
+        hideUploadSet();
+    } else {
+        showUploadSet();
+    }
+}
+
+function changeSelectedSet(selectedSet) {
+    setSelected = selectedSet;
+    hideUploadSet()
+    showUploadSet()
+}
+
+function showUploadSet() {
+    let iconsContainer = document.getElementsByClassName("upload-icons")[0]
+    if (setSelected) {
+        document.getElementById("postSelectedSet").style.display = "flex"
+        return
+    }
+    for (let i = 0; i < 4; i++) {
+        iconsContainer.children[i].style.display = "unset"
+    }
+}
+
+hideUploadSet()
+function hideUploadSet() {
+    let iconsContainer = document.getElementsByClassName("upload-icons")[0]
+    for (let i = 0; i < 4; i++) {
+        iconsContainer.children[i].style.display = "none"
+    }
+    document.getElementById("postSelectedSet").style.display = "none"
+}
